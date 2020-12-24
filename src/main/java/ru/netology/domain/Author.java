d58701da035185daf4e4e4bf0437ea2d6a1cb7b0
kos-vkg <kvertenov@mail.ru>
2020-12-25 01:47:10 +0300

    v1

---
+++ src/main/java/ru/netology/domain/Author.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class Author {
+    private int idAuhtor;
+    private String login;
+    private String password;
+    private String dateRegistation;
+    private String name;
+}
