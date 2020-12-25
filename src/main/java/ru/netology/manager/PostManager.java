f17b04fcdd7e07a37df0cf86be078d7349cde046
kos-vkg <kvertenov@mail.ru>
2020-12-25 16:04:35 +0300

    task2

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -10,12 +10,13 @@ public class PostManager {
         return null;
     }
 
-    public Post[] search() {
+    public Post[] search( int ownerId,String domain, String query,boolean ownersOnly,int count,int offset,boolean extended ) {
         // TODO: add logic
         return null;
     }
 
-    public void delete() {
+
+    public void delete(int ownerId, int postId   ) {
         // TODO: add logic
     }
 }
